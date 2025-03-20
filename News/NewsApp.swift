e568ac73b49b7292fdbf63969af30aa05332825f
Victoria Miroshnik <victoriafreeman990@gmail.com>
2025-03-20 23:02:38 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Виктория Мирошник on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsTests/NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTests.swift
+//  NewsTests
+//
+//  Created by Виктория Мирошник on 20.03.2025.
+//
+
+import Testing
+@testable import News
+
+struct NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
